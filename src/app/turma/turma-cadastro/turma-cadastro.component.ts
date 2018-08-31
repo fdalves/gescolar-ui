93cf690ed1ffc1af4794a0d419c318f0338c753c
Francisco D' Avila Alves <francisco.d.alves@gmail.com>
2018-08-31 16:27:21 -0300

    teste
    teste

--- src/app/turma/turma-cadastro/turma-cadastro.component.ts
+++ src/app/turma/turma-cadastro/turma-cadastro.component.ts
@@ -3,9 +3,9 @@ import { GrowMessageService } from './../../shared/grow-message.service';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ErrorHandlerService } from './../../core/error-handler.service';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators, FormArray } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import {SelectItem} from 'primeng/api';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-turma-cadastro',
@@ -17,60 +17,49 @@ export class TurmaCadastroComponent implements OnInit {
   formulario: FormGroup;
   series = [];
   turnos = [];
-  periodos = [{nome: 'Segunda', value: 'SEGUNDA', quant: 5 },
-              {nome: 'Terça', value: 'TERCA', quant: 5 },
-              {nome: 'Quarta', value: 'TERCA', quant: 5 },
-              {nome: 'Quinta', value: 'TERCA', quant: 5 },
-              {nome: 'Sexta', value: 'TERCA', quant: 5 }];
 
   quantDiasSemana = 5;
-  diasSem: SelectItem [];
+  diasSem: SelectItem[];
 
-  constructor( private errorHandler: ErrorHandlerService,
-                private route: ActivatedRoute,
-                private router: Router,
-                private title: Title,
-                private messageService: GrowMessageService,
-                private fb: FormBuilder,
-                private turmaService: TurmaService) { }
+  constructor(private errorHandler: ErrorHandlerService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private title: Title,
+    private messageService: GrowMessageService,
+    private fb: FormBuilder,
+    private turmaService: TurmaService) { }
 
   ngOnInit() {
 
     this.quantDiasSemana = 5;
     this.diasSem = [
-      {label: '5 dias de Aula Por Semana', value: 5},
-      {label: '6 dias de Aula Por Semana', value: 6}];
-
-    
-
+      { label: '5 dias de Aula Por Semana', value: 5 },
+      { label: '6 dias de Aula Por Semana', value: 6 }];
     const codigoTurma = this.route.snapshot.params['codigo'];
     this.configuraFormulario();
     this.title.setTitle('Turma');
-
-
     if (codigoTurma) {
       this.carregarTurma(codigoTurma);
     }
-
     this.initSeries();
     this.initTurno();
-
+    this.addItem('Segunda', 'SEGUNDA', 5);
+    this.addItem('Terça', 'TERCA', 5);
+    this.addItem('Quarta', 'QUARTA', 5);
+    this.addItem('Quinta', 'QUINTA', 5);
+    this.addItem('Sexta', 'SEXTA', 5);
   }
 
   onChange(event: any) {
-
-    if (this.formulario.get('quantidadeDiasSemana') && 
-        this.formulario.get('quantidadeDiasSemana').value === 5 &&
-        this.periodos.length === 6) {
-          this.periodos.splice(5,1);
-    
-        }else if (this.formulario.get('quantidadeDiasSemana') && 
-        this.formulario.get('quantidadeDiasSemana').value === 6 && 
-        this.periodos.length === 5){
-          this.periodos.push({nome: 'Sábado', value: 'TERCA', quant: 5 });
-        }  
-
-   
+    if (this.formulario.get('quantidadeDiasSemana') &&
+      this.formulario.get('quantidadeDiasSemana').value === 5 &&
+      this.getPeriodos().controls.length === 6) {
+      this.getPeriodos().controls.splice(5, 1);
+    } else if (this.formulario.get('quantidadeDiasSemana') &&
+      this.formulario.get('quantidadeDiasSemana').value === 6 &&
+      this.getPeriodos().controls.length === 5) {
+      this.addItem('Sábado', 'SABADO', 5);
+    }
   }
 
   configuraFormulario() {
@@ -82,12 +71,25 @@ export class TurmaCadastroComponent implements OnInit {
       'sala': [],
       'vagas': [],
       'quantidadeDiasSemana': 5,
-      'periodos':  [{nome: 'Segunda', value: 'SEGUNDA', quant: 5 },
-              {nome: 'Terça', value: 'TERCA', quant: 5 },
-              {nome: 'Quarta', value: 'TERCA', quant: 5 },
-              {nome: 'Quinta', value: 'TERCA', quant: 5 },
-              {nome: 'Sexta', value: 'TERCA', quant: 5 }]
+      'periodos': this.fb.array([])
     });
+  }
+
+  createItem(nome: string, value: string, quant: number): FormGroup {
+    return this.fb.group({
+      nome: nome,
+      value: value,
+      quant: quant
+    });
+  }
+
+  addItem(nome: string, value: string, quant: number): void {
+    this.getPeriodos().push(this.createItem(nome, value, quant));
+  }
+
+
+  getPeriodos(): FormArray {
+    return this.formulario.get('periodos') as FormArray;
   }
 
   carregarTurma(codigo: number) {
@@ -107,16 +109,16 @@ export class TurmaCadastroComponent implements OnInit {
 
   public initSeries() {
     return this.turmaService.listarTodasSeries()
-    .then(series => {
-      this.series = series;
-    })
-    .catch(erro => this.errorHandler.handle(erro));
+      .then(series => {
+        this.series = series;
+      })
+      .catch(erro => this.errorHandler.handle(erro));
   }
 
   public initTurno() {
-    this.turnos[0] = {label: 'Manhã', value: 'MANHA' };
-    this.turnos[1] = {label: 'Tarte', value: 'TARDE' };
-    this.turnos[2] = {label: 'Noite', value: 'NOITE' };
+    this.turnos[0] = { label: 'Manhã', value: 'MANHA' };
+    this.turnos[1] = { label: 'Tarte', value: 'TARDE' };
+    this.turnos[2] = { label: 'Noite', value: 'NOITE' };
   }
 
 }
