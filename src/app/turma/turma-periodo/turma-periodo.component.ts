acf3592360e75c8770594161f204d59abe24a756
francisco <francisco.d.alves@gmail.com>
2018-12-05 01:11:27 -0200

    dfsdf
    sdfsdf

--- src/app/turma/turma-periodo/turma-periodo.component.ts
+++ src/app/turma/turma-periodo/turma-periodo.component.ts
@@ -6,6 +6,7 @@ import { ErrorHandlerService } from './../../core/error-handler.service';
 
 
 
+
 @Component({
   selector: 'app-turma-periodo',
   templateUrl: './turma-periodo.component.html',
@@ -70,7 +71,7 @@ export class TurmaPeriodoComponent implements OnInit {
     return this.turmaService.listarDisciplinas()
       .then(disciplinas => {
         this.disciplinas = disciplinas.
-          map(t => ({ label: t.nome, value: t.codigo }));
+          map(t => ({ label: t.nome, value: {id: t.codigo, name: t.nome} }));
       })
       .catch(erro => this.errorHandler.handle(erro));
   }
@@ -80,12 +81,74 @@ export class TurmaPeriodoComponent implements OnInit {
     return this.professorService.listarTodas()
       .then(professores => {
         this.professores = professores.
-          map(t => ({ label: t.nome, value: t.codigo }));
+          map(t => ({ label: t.nome, value: {id: t.codigo, name: t.nome} }));
         })
       .catch(erro => this.errorHandler.handle(erro));
   }
 
   salvar(form: FormControl) {
+    for (const periodo of  this.periodosSelecionado) {
+        if (periodo.dia === 'SEGUNDA') {
+          for (const periodoSegunda of  this.segunda) {
+            if (periodo.periodo === periodoSegunda.periodo) {
+                 const disciplinaTurma = this.getDiciplinaTurma();
+                 this.removeVagos(periodoSegunda);
+                 periodoSegunda.disciplinaTurma = disciplinaTurma;
+              }
+          }
+        }
+
+        if (periodo.dia === 'TERCA') {
+          for (const periodoTerca of  this.terca) {
+            if (periodo.periodo === periodoTerca.periodo) {
+                 const disciplinaTurma = this.getDiciplinaTurma();
+                 this.removeVagos(periodoTerca);
+                 periodoTerca.disciplinaTurma = disciplinaTurma;
+              }
+          }
+        }
+
+        if (periodo.dia === 'QUARTA') {
+          for (const periodoQuarta of  this.quarta) {
+            if (periodo.periodo === periodoQuarta.periodo) {
+                 const disciplinaTurma = this.getDiciplinaTurma();
+                 this.removeVagos(periodoQuarta);
+                 periodoQuarta.disciplinaTurma = disciplinaTurma;
+              }
+          }
+        }
+
+        if (periodo.dia === 'QUINTA') {
+          for (const periodoQinta of  this.quinta) {
+            if (periodo.periodo === periodoQinta.periodo) {
+                 const disciplinaTurma = this.getDiciplinaTurma();
+                 this.removeVagos(periodoQinta);
+                 periodoQinta.disciplinaTurma = disciplinaTurma;
+              }
+          }
+        }
+
+        if (periodo.dia === 'SEXTA') {
+          for (const periodoSexta of  this.sexta) {
+            if (periodo.periodo === periodoSexta.periodo) {
+                 const disciplinaTurma = this.getDiciplinaTurma();
+                 this.removeVagos(periodoSexta);
+                 periodoSexta.disciplinaTurma = disciplinaTurma;
+              }
+          }
+        }
+
+        if (periodo.dia === 'SABADO') {
+          for (const periodoSabado of  this.sabado) {
+            if (periodo.periodo === periodoSabado.periodo) {
+                 const disciplinaTurma = this.getDiciplinaTurma();
+                 this.removeVagos(periodoSabado);
+                 periodoSabado.disciplinaTurma = disciplinaTurma;
+              }
+          }
+        }
+    }
+    this.fecharPopUp();
   }
 
   limparPopUp() {
@@ -98,4 +161,25 @@ export class TurmaPeriodoComponent implements OnInit {
     this.limparPopUp();
     this.exbindoFormulario = false;
   }
+
+
+  removeVagos(periodo: any) {
+    for (const periodoVago of this.periodosVagos) {
+      if (periodoVago.dia === periodo.dia && periodoVago.periodo === periodo.periodo) {
+        const index = this.periodosVagos.findIndex(p => p === periodoVago);
+        console.log(index);
+        this.periodosVagos.splice(index, 1);
+      }
+    }
+  }
+
+  getDiciplinaTurma(): any {
+    const disciplinaTurma = {'codigoTurma': this.codigoTurma,
+                            'codigoProfessor': this.professorSelecionado.id,
+                            'nomeProfessor': this.professorSelecionado.name,
+                            'codigoDisciplina': this.disciplinaSelecionada.id,
+                            'nomeDisciplina': this.disciplinaSelecionada.name};
+    return disciplinaTurma;
+  }
+
 }
